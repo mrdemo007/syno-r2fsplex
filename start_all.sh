05c5603a9f1de5a6303a803a1ebbe207da080152
Jonas A <demo007@gmail.com>
2021-04-23 23:56:23 +0200

    Update start_all.sh

--- start_all.sh
+++ start_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/local/bin/rar2fs -o ro,allow_other -o auto_unmount --seek-length=1 /nomorerar /data
-/usr/lib/plexmediaserver/lib/Plex Media Server
+/usr/lib/plexmediaserver/Plex\ Media\ Server
